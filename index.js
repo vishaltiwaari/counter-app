4b29a2d315397c68d137b2a536d57534c7c8fca4
Vishal Tiwari <113412456+vishaltiwaari@users.noreply.github.com>
2023-03-17 14:10:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ function increment() {
 
 function save() {
     let countStr = count + " - "
-    saveEl.textContenttStr
+    saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
 }
 
 console.log("Let's count people on the subway!")
+
